778cfe3ab7119f91ba979e76b04b4c77f115e3c0
Zaman3027 <zamanapp@outlook.com>
2019-01-07 02:04:29 +0530

    Add files via upload

---
+++ build/precache-manifest.95e7168a77e1782fc3b1e0b40871b651.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39f7ac85f678ee891117",
+    "url": "/static/js/main.39f7ac85.chunk.js"
+  },
+  {
+    "revision": "34560d990f583c84080a",
+    "url": "/static/js/1.34560d99.chunk.js"
+  },
+  {
+    "revision": "39f7ac85f678ee891117",
+    "url": "/static/css/main.4b999c32.chunk.css"
+  },
+  {
+    "revision": "3656646d95b6a3d998c20593999a1f45",
+    "url": "/index.html"
+  }
+];
