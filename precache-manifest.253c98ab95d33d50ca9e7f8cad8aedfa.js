3765a835deac807893545e369e291233631f5b0a
Mahafuz <zamanapp@outlook.com>
2019-01-07 02:21:51 +0530

    Updates

---
+++ precache-manifest.253c98ab95d33d50ca9e7f8cad8aedfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "474c9fae5c6820b5e40a",
+    "url": "/movieList/static/js/runtime~main.474c9fae.js"
+  },
+  {
+    "revision": "c7eb838125fd3f82467e",
+    "url": "/movieList/static/js/main.c7eb8381.chunk.js"
+  },
+  {
+    "revision": "34560d990f583c84080a",
+    "url": "/movieList/static/js/1.34560d99.chunk.js"
+  },
+  {
+    "revision": "c7eb838125fd3f82467e",
+    "url": "/movieList/static/css/main.4b999c32.chunk.css"
+  },
+  {
+    "revision": "416ffb31b4f09bebcf886eff30950680",
+    "url": "/movieList/index.html"
+  }
+];
